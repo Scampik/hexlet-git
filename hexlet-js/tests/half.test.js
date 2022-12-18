a4fcd04f590aa19c0c24fd95935686e4d8cdfa4f
Ildar Sahautdinov <scams1992@gmail.com>
2022-12-18 15:54:00 +0500

    add asserts test

--- hexlet-js/tests/half.test.js
+++ hexlet-js/tests/half.test.js
@@ -1,11 +1,7 @@
 import capitalize from '../src/half.js';
+import { strict as asserts} from 'node:assert';
 
-if (capitalize('') !== '') {
-  throw new Error('Mda');
-}
-
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Mda');
-}
+asserts.equal(capitalize(''), '');
+asserts.equal(capitalize('hello'), 'Hesllo');
 
 console.log('Все тесты пройдены!');
